b611792069faa79e272f551546fdaa8789491573
Mpho Mosima <157699136+DevMpho@users.noreply.github.com>
2025-04-04 14:17:53 +0200

    Created new files and MVC

---
+++ Laptops/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laptops.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
