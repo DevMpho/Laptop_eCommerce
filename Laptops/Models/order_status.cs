6626e664db485ff5b600879480dcf3e61d972b0f
23026475 <ndivhuswiswi@gmail.com>
2025-05-23 19:30:19 +0200

    changed status_id to order_status_id

--- Laptops/Models/order_status.cs
+++ Laptops/Models/order_status.cs
@@ -5,7 +5,7 @@ namespace Laptops.Models
     public class order_status
     {
         [Key]
-        public int status_id { get; set; }
+        public int order_status_id { get; set; }
         public string status_name { get; set; }
     }
 }
