3c4a3e4e6960f4b4ac656c8f24a5da29da23984b
23026475 <ndivhuswiswi@gmail.com>
2025-05-24 15:44:47 +0200

    the model for the display of oerdetails

---
+++ Laptops/Models/OrderDetailsViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Laptops.Models
+{
+    public class OrderDetailsViewModel
+    {
+        // Order info
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int TotalAmount { get; set; }
+        public int OrderStatusId { get; set; }
+        public string Status { get; set; } = string.Empty; // From OrderStatus table
+
+        // Employee info
+        public string EmployeeName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public long? ContactNumber { get; set; }
+
+
+        // Laptops in the order
+        public List<LaptopViewModel> Laptops { get; set; } = new();
+    }
+}
